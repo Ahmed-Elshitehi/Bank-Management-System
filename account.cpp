cebfddfed4478c7f36aa1e277c43e795bb20b972
Ahmed Salah <elshitehi1@gmail.com>
2024-02-12 01:18:51 +0200

    account

---
+++ account.cpp
@@ -0,0 +1,32 @@
+
+#include "account.h"
+
+account::account() : password(""), balance(0), id(++current_id) {
+
+}
+
+int account::current_id = 0;
+
+const int &account::Get_id() const {
+    return id;
+}
+
+const int &account::Get_balance() const {
+    return balance;
+}
+
+const std::string &account::Get_password() const {
+    return password;
+}
+
+void account::set_id(int id) {
+    this->id = id;
+}
+
+void account::set_balance(int balance) {
+    this->balance = balance;
+}
+
+void account::set_password(std::string password) {
+    this->password = password;
+}
---
+++ account.h
@@ -0,0 +1,21 @@
+#ifndef BANK_MANAGEMENT_SYSTEM_ACCOUNT_H
+#define BANK_MANAGEMENT_SYSTEM_ACCOUNT_H
+#include <iostream>
+class account {
+private:
+    int id;
+    std::string password;
+    int balance;
+    static int current_id;
+public:
+    account();
+    void set_id(int id);
+    void set_balance(int balance);
+    void set_password(std::string password);
+    const int &Get_id() const;
+    const int &Get_balance() const;
+    const std::string &Get_password() const;
+};
+
+
+#endif
