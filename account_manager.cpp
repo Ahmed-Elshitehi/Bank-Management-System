7f01dba2248ee2bebb437c277f26e0339607fd6d
Ahmed Salah <elshitehi1@gmail.com>
2024-02-12 14:17:35 +0200

    signup

--- account_manager.cpp
+++ account_manager.cpp
@@ -33,3 +33,14 @@ account_manager::~account_manager() {
     accounts.clear();
     current_account = nullptr;
 }
+
+void account_manager::SignUp() {
+    current_account = new account;
+    std::string str;
+    std::cout << "Enter user name (No spaces): ";
+    std::cin >> str;
+    current_account->Set_user_name(str);
+    std::cout << "Enter password (no spaces): ";
+    std::cin >> str;
+    current_account->Set_password(str);
+}
