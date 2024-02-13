70746cb261d3cc7aa95bfca262f1a85fe803c947
Ahmed Salah <elshitehi1@gmail.com>
2024-02-13 14:37:35 +0200

    update system

--- system.cpp
+++ system.cpp
@@ -5,9 +5,12 @@ system::system() : accountManager(new account_manager()) {
 }
 
 void system::Run() {
-    while (true) {
+    char choose;
+    do {
         accountManager->AccessSystem();
-    }
+        std::cout << "DO You need any another operations? (Y/N) :";
+        std::cin >> choose;
+    } while (toupper(choose) == 'Y');
 }
 
 system::~system() {
